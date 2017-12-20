8da9fe90f11f760e1ae8b2a629d16d006ec1a670
Simon Moll <moll@cs.uni-saarland.de>
2017-12-20 16:30:58 +0100

    Recursive DAG Network

--- src/model.py
+++ src/model.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 import numpy as np
 
+
+
+def data_type():
+    return tf.float32
+
 with tf.Session() as sess:
     a = tf.Variable(5.0, name='a')
     b = tf.Variable(6.0, name='b')
@@ -12,7 +17,30 @@ with tf.Session() as sess:
     print(b.eval()) # 6.0
     print(c.eval()) # 30.0
 
+    tf.train.write_graph(sess.graph_def, 'build/', 'toy_graph.pb', as_text=False)
+
+# number of distinct opcodes
+num_codes = 9
+
+# most basic version -> operate over a chain of op codes (just for testing)
+with tf.Session() as sess:
+    batch_size = 256
+    num_features = 1
+    lstm_size = 256
+
+    # op code translation
+    oc_data = tf.placeholder(data_type(), [None, batch_size, num_features], name = "oc")
+
+    with tf.device("/cpu:0"):
+      embedding = tf.get_variable("embedding", [num_codes, lstm_size], dtype=data_type())
+      inputs = tf.nn.embedding_lookup(embedding, oc_data)
+
+    # sess.run(tf.global_variables_initializer())
+
+    lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+
     tf.train.write_graph(sess.graph_def, 'build/', 'apo_graph.pb', as_text=False)
+
 
 ## advanced setup ##
 ## input sequence of statements:
