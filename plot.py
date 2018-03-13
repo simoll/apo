df378f4fdff7732d49ffb4418aade03652ec9497
Simon Moll <moll@cs.uni-saarland.de>
2018-03-13 20:19:59 +0100

    parse better solutions

--- plot.py
+++ plot.py
@@ -12,16 +12,24 @@ def parseRoundHeader(l):
 # Greedy (best)  0.8790  (matched 0.8550, longerDer 0.0020, shorterDer: 0.0030, betterScore: 0.0190)
 def parseEvalLine(l):
   # "best)  0.8790  "
-  seg = l.split('(')
-  leftParts = seg[1].split(' ')
-  totalText = leftParts[-3] 
-  total = float(totalText)
-  return total
+  parts = l.split("matched")
+  leftParts = parts[0][:-1].strip().split(" ")
+  totalText = leftParts[-1] 
+
+  detailParts = parts[1][:-1].split(' ')
+  betterText = detailParts[-1][:-1]
+  print(betterText)
+
+  return float(totalText), float(betterText)
 
 def parseLog(logPath):
   X = []
   bestY = []
+  bestBetterY = []
   stopY = []
+  stopBetterY = []
+  incBetterY = []
+
   trainRound=None
   for l in open(logPath, 'r'):
     if l.startswith("- Round "):
@@ -32,19 +40,32 @@ def parseLog(logPath):
         print("Missed round header!!!")
         raise SystemExit
       # total, match, longerDer, shortedDer, betterScore = parseEvalLine(l)
-      bestTotal = parseEvalLine(l)
+      bestTotal, bestBetter = parseEvalLine(l)
       bestY.append(bestTotal)
+      bestBetterY.append(bestBetter)
     elif "(STOP)" in l:
       if trainRound is None:
         print("Missed round header!!!")
         raise SystemExit
       # total, match, longerDer, shortedDer, betterScore = parseEvalLine(l)
-      stopTotal = parseEvalLine(l)
+      stopTotal, stopBetter = parseEvalLine(l)
       stopY.append(stopTotal)
-  return X, bestY, stopY
+      stopBetterY.append(stopBetter)
+    elif "Incumbent" in l:
+      if trainRound is None:
+        print("Missed round header!!!")
+        raise SystemExit
+      # total, match, longerDer, shortedDer, betterScore = parseEvalLine(l)
+      incTotal, incBetter = parseEvalLine(l)
+      # stopY.append(stopTotal)
+      incBetterY.append(incBetter)
+  return X, bestY, bestBetterY, stopY, stopBetterY, incBetterY
 
 
-X, bestY, stopY = parseLog(sys.argv[1])
+X, bestY, bestBetterY, stopY, stopBetterY, incBetterY = parseLog(sys.argv[1])
 pl.plot(X, bestY, 'b')
+pl.plot(X, bestBetterY, 'c')
 pl.plot(X, stopY, 'g')
+pl.plot(X, stopBetterY, 'm')
+pl.plot(X, incBetterY, 'r')
 pl.show()
